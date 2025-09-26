9fc1e6d3cce9727c27c61334cbd3c9c13c740b3f
ola-nishant <nishant.4716403221@ipu.ac.in>
2025-09-26 16:02:47 +0530

    Added rules and instructions

--- src/components/PuzzleFlow.tsx
+++ src/components/PuzzleFlow.tsx
@@ -47,12 +47,22 @@ export default function PuzzleFlow() {
       <Toaster position="top-center" />
       {step === 0 && (
         <div className="p-6 text-white rounded-xl shadow-md text-center">
-          <h1 className="bg-clip-text text-transparent text-center bg-gradient-to-b from-neutral-900 to-neutral-700 dark:from-neutral-600 dark:to-white text-4xl lg:text-7xl font-sans py-2 md:py-10 relative z-20 font-bold tracking-tight">
+          <h1 className="bg-clip-text text-transparent text-center bg-gradient-to-b from-neutral-900 to-neutral-700 dark:from-neutral-600 dark:to-white text-4xl lg:text-7xl font-sans py-2 md:py-5 relative z-20 font-bold tracking-tight">
             Culture Quest
           </h1>
-          <div className="flex justify-center">
+          <p className="max-w-2xl mx-auto text-sm md:text-md text-neutral-700 dark:text-neutral-400 text-center">
+            Rules of the game are:
+            <br /> 1. There will be 4 puzzles.
+            <br /> 2. Solving each puzzle will give you a clue, REMEMBER IT!
+            <br /> 3. Combine all the clues and escape to win.
+          </p>
+          <div className="flex justify-center mt-5">
             <HoverBorderGradient
-              onClick={() => setStep(1)} // Set the step to 1 when clicked
+              onClick={() => {
+                setTimeout(() => {
+                  setStep(1); // Set the step to 1 after a 500ms delay
+                }, 750);
+              }}
               className="px-6 py-2 text-white text-sm md:text-lg relative z-30"
             >
               Start Puzzle
