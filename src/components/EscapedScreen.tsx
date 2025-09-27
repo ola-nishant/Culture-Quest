0160a133ce22d28f714ef08038ae99dac0d757e3
ola-nishant <nishant.4716403221@ipu.ac.in>
2025-09-27 22:41:47 +0530

    Fixed build errors

--- src/components/EscapedScreen.tsx
+++ src/components/EscapedScreen.tsx
@@ -4,17 +4,17 @@ import { BackgroundLines } from "./ui/background-lines";
 export default function EscapedScreen() {
   return (
     <div className="relative flex min-h-screen items-center justify-center text-white">
-      {/* Background Ripple Effect */}
+
       <div className="absolute inset-0 -z-20">
         <BackgroundRippleEffect />
       </div>
 
-      {/* Background Lines */}
+
       <div className="absolute inset-0 -z-10">
         <BackgroundLines />
       </div>
 
-      {/* Foreground Text */}
+
       <div className="relative z-10 text-center">
         <h1 className="bg-clip-text text-transparent bg-gradient-to-b from-neutral-900 to-neutral-700 dark:from-neutral-600 dark:to-white text-4xl lg:text-7xl font-sans py-2 md:py-5 font-bold tracking-tight">
           You Escaped!
