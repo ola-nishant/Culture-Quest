b6c60bb8f7828cba961c996b1d56b40db0c5cb7f
ola-nishant <nishant.ola22@gmail.com>
2025-10-22 23:44:25 +0530

    restore bg color

--- src/components/ui/background-ripple-effect.tsx
+++ src/components/ui/background-ripple-effect.tsx
@@ -24,7 +24,7 @@ export const BackgroundRippleEffect = ({
       className={cn(
         "absolute inset-0 h-full w-full",
         "[--cell-border-color:var(--color-neutral-300)] [--cell-fill-color:var(--color-neutral-100)] [--cell-shadow-color:var(--color-neutral-500)]",
-        "dark:[--cell-border-color:grey)] dark:[--cell-fill-color:yellow] dark:[--cell-shadow-color:blue]"
+        "dark:[--cell-border-color:var(--color-neutral-700)] dark:[--cell-fill-color:var(--color-neutral-900)] dark:[--cell-shadow-color:var(--color-neutral-800)]",
       )}
     >
       <div className="relative h-full w-full overflow-hidden">
